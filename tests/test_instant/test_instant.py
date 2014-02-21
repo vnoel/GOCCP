02a9ee8552efcc669f7b6f354cb8846a77f93270
Vincent Noel <vincent.noel@gmail.com>
2014-02-21 14:26:11 +0100

    work on test suite

--- tests/test_instant/test_instant.py
+++ tests/test_instant/test_instant.py
@@ -21,14 +21,28 @@ def compare_variable(nc1, nc2, var):
         return False
         
     print var, ': ok.'
+    return True
 
 
-def test_instant(file1, file2):
+def test_nonzero(nc2, var):
+    
+    var2 = nc2.variables[var]
+
+    if np.all(var2[:]==0):
+        print 'All points in ', var, ' are zero'
+        return False
+    print var, ': content appears ok.'
+
+    return True
+
+
+def compare_instant(file2, file1='./reference/instant_SR_CR_DR_2007-01-01T00-22-49ZN_night_CFMIP2_2.65.nc'):
     
     variables = ['longitude', 'latitude', 'alt_mid', 'alt_bound', 'time', 'SE', 
                 'instant_SR', 'instant_CR', 'instant_DR',
                 'ATB', 'ATB_mol', 'ATB_per', 'ATB_par', 'TEMP']
         
+
     print 'Comparing :'
     print file1
     print file2
@@ -38,16 +52,32 @@ def test_instant(file1, file2):
     
     for var in variables:
         compare_variable(nc1, nc2, var)
+        
+    nc1.close()
+    nc2.close()
 
+
+def test_instant(file2):
+
+    variables = ['longitude', 'latitude', 'alt_mid', 'alt_bound', 'time', 'SE', 
+                'instant_SR', 'instant_CR', 'instant_DR',
+                'ATB', 'ATB_mol', 'ATB_per', 'ATB_par', 'TEMP']
+
+    print 'Testing content validity of ', file2
+    nc2 = netCDF4.Dataset(file2)
+
+    for var in variables:
+        test_nonzero(nc2, var)
+        
+    nc2.close()
 
 def main():
 
-    # reference file
-    file1 = './reference/instant_SR_CR_DR_2007-01-01T00-22-49ZN_night_CFMIP2_2.65.nc'
     # file to compare
     file2 = '../../instant/instant_SR_CR_DR_2007-01-01T00-22-49ZN_night_CFMIP2_2.70.nc'
 
-    test_instant(file1, file2)
+    compare_instant(file2)
+    test_instant(file2)
 
     print 'tests over.'
 
