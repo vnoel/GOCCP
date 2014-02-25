e17e841295ab3328db992a0c5556d1efe0e0c438
Vincent Noel <vincent.noel@gmail.com>
2014-02-25 14:34:04 +0100

    view SR_histo files

--- vis/view.py
+++ vis/view.py
@@ -3,6 +3,7 @@
 import netCDF4
 import os, sys
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 def maps_show(ncfile, variables):
@@ -26,6 +27,30 @@ def maps_show(ncfile, variables):
         
     plt.show()
     
+    
+def srhisto_show(ncfile):
+    
+    print 'Opening ', ncfile
+    nc = netCDF4.Dataset(ncfile)
+    
+    lon = nc.variables['longitude'][:]
+    lat = nc.variables['latitude'][:]
+    # cfad_lidarsr532_Occ(time, box, altitude, latitude, longitude)
+    cfad = nc.variables['cfad_lidarsr532_Occ'][0,:]
+    
+    cfad = np.ma.masked_invalid(cfad)
+    cfad = cfad.mean(axis=3)
+    cfad = cfad.mean(axis=2)
+    # cfad[box, altitude]
+    
+    plt.figure()
+    plt.pcolormesh(cfad.T)
+    plt.colorbar()
+    plt.clim(0,50)
+    plt.title('cfad_lidarsr532_Occ')
+    
+    plt.show()
+    
 
 def main(filepath):
     
@@ -40,6 +65,8 @@ def main(filepath):
             variables = ['clhcalipso', 'bascalipso', 'topcalipso']
 
         maps_show(filepath, variables)
+    if ncfile.startswith('SR_histo'):
+        srhisto_show(filepath)
 
 
 if __name__ == '__main__':
